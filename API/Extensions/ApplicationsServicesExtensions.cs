af10c46945f8ccc24a1129f4a9d19b50f40a6193
Alexey Koloty <alexeykoloty@Vegas.local>
2024-06-25 10:12:21 +0300

    Searching, Sorting, Pagination and Cors

--- API/Extensions/ApplicationsServicesExtensions.cs
+++ API/Extensions/ApplicationsServicesExtensions.cs
@@ -41,6 +41,17 @@ namespace API.Extensions
                 };
             });
 
+            services.AddCors(options => 
+            {
+                options.AddPolicy("CorsPolicy", policy => 
+                {   
+                    policy
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .WithOrigins("https://localhost:4200");
+                });
+            });
+
             return services;
         }
     }
--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -5,10 +5,6 @@ namespace Core.Specifications
     public class BaseSpecification<T> : ISpecification<T>
     {
 
-        public Expression<Func<T, bool>> Criteria { get; }
-        public List<Expression<Func<T, object>>> Includes { get; } = 
-            new List<Expression<Func<T, object>>>();
-
         public BaseSpecification(Expression<Func<T, bool>> criteria)
         {
             Criteria = criteria;
@@ -18,8 +14,41 @@ namespace Core.Specifications
         {
         }
 
-        protected void AddInclide(Expression<Func<T, object>> includeExpression){
+        public Expression<Func<T, bool>> Criteria { get; }
+        public List<Expression<Func<T, object>>> Includes { get; } =
+            new List<Expression<Func<T, object>>>();
+
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
+        public int Take { get; private set;}
+
+        public int Skip { get; private set;}
+
+        public bool IsPagingEnabled { get; private set;}
+
+        protected void AddInclide(Expression<Func<T, object>> includeExpression)
+        {
             Includes.Add(includeExpression);
+        }
+
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        {
+            OrderByDescending = orderByDescExpression;
+        }
+
+        protected void ApplyPaging(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
         }
 
     }
